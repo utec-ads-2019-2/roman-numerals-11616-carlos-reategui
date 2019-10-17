cf9490da3349ad43eaa59017305e5f57e4539ae1
carlos-reategui <39816970+carlos-reategui@users.noreply.github.com>
2019-10-17 13:47:22 -0500

    accepted

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 #include <unordered_map>
 using namespace std;
 
+typedef unsigned int ui;
+
 int main() {
-    unordered_map<char, short unsigned int> letterToInt = {{'I', 1}, {'V', 5}, {'X', 10},
+    unordered_map<char, ui> letterToInt = {{'I', 1}, {'V', 5}, {'X', 10},
                                                             {'L', 50}, {'C', 100}, {'D', 500},
                                                             {'M', 1000}};
-    unordered_map<short unsigned int, char> intToLetter = {{1, 'I'}, {5, 'V'}, {10, 'X'},
+    unordered_map<ui, char> intToLetter = {{1, 'I'}, {5, 'V'}, {10, 'X'},
                                                            {50, 'L'}, {100, 'C'}, {500, 'D'},
                                                            {1000, 'M'}};
     string line = "line";
@@ -24,7 +26,7 @@ int main() {
             }
             cout << output << endl;
         } else {
-            unsigned int base = 1;
+            unsigned int base;
             switch (line.size()) {
                 case 4: base = 1000; break;
                 case 3: base = 100; break;
@@ -32,43 +34,29 @@ int main() {
                 case 1: base = 1; break;
             }
             string output;
-            for (unsigned long i = line.size() - 1; i >= 0; --i) {
-                //cout << "ok" << endl;
-                switch (stoi(to_string(line[i]))) {
-                    case 1:
-                    case 2:
-                    case 3:
-                        //cout << "ok2" << endl;
-                        for (unsigned int j = 0; j < stoi(string(1, line[i])); ++j)
-                            output += intToLetter[1 * base];
-                        //cout << "ok3" << endl;
-                        break;
-                    case 4:
-                        output += intToLetter[1 * base] + intToLetter[5 * base];
-                        break;
-                    case 5:
-                        output += intToLetter[5 * base];
-                        break;
-                    case 6:
-                    case 7:
-                    case 8:
-                        output += intToLetter[5 * base];
-                        for (unsigned int j = 0; j < stoi(string(1, line[i])) - 5; ++j)
-                            output += intToLetter[1 * base];
-                        break;
-                    case 9:
-                        output += intToLetter[1 * base] + intToLetter[10 * base];
-                        break;
-                }
-                if (stoi(to_string(line[i])) < 4) {
-                    for (unsigned int j = 0; j < stoi(string(1, line[i])); ++j)
+            for (char i : line) {
+                ui iValue = int(i) - '0';
+                if (iValue < 4) {
+                    for (ui j = 0; j < iValue; ++j)
                         output += intToLetter[1 * base];
+                } else if (iValue == 4) {
+                    output += intToLetter[1 * base];
+                    output += intToLetter[5 * base];
+                } else if (iValue == 5) {
+                    output += intToLetter[5 * base];
+                } else if (iValue > 5 and iValue < 9) {
+                    output += intToLetter[5 * base];
+                    for (ui j = 0; j < iValue - 5; ++j) {
+                        output += intToLetter[1 * base];
+                    }
+                } else if (iValue == 9) {
+                    output += intToLetter[1 * base];
+                    output += intToLetter[10 * base];
                 }
                 base /= 10;
             }
             cout << output << endl;
         }
     }
-    //cout << stoi(string(1, '4'));
     return EXIT_SUCCESS;
 }
